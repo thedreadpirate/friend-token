180523331265c38da635c02282746261c9ca8831
Keith OBrien <kdobrien@us.ibm.com>
2014-12-15 11:46:46 -0500

    tests were referring to the generate-key method which was moved to a different file

--- test/tombooth/friend_token/token_store_test.clj
+++ test/tombooth/friend_token/token_store_test.clj
@@ -1,26 +1,27 @@
 (ns tombooth.friend-token.token-store-test
   (:require [clojure.test :refer :all]
             [tombooth.friend-token.token :as token]
+            [tombooth.friend-token :as workflow]
             [tombooth.friend-token.token-store :as store]))
 
 (deftest create-test
   (testing "Test creating a token"
     (let [tokens (atom {})
-          token-store (store/->MemTokenStore (token/generate-key) 10 tokens)]
+          token-store (store/->MemTokenStore (workflow/generate-key) 10 tokens)]
       (store/create token-store {:username "foo"})
       (is (= 1 (count (keys @tokens)))))))
 
 (deftest get-metadata-test
   (testing "Test we can get metadata"
     (let [tokens (atom {})
-          token-store (store/->MemTokenStore (token/generate-key) 10 tokens)
+          token-store (store/->MemTokenStore (workflow/generate-key) 10 tokens)
           token-hex (store/create token-store {:username "foo"})]
       (is (store/get-metadata token-store token-hex)))))
 
 (deftest valid-fail-ttl-test
   (testing "Test getting metadata fails if outside ttl"
     (let [tokens (atom {})
-          token-store (store/->MemTokenStore (token/generate-key) 1 tokens)
+          token-store (store/->MemTokenStore (workflow/generate-key) 1 tokens)
           token-hex (store/create token-store {:username "foo"})]
       (Thread/sleep 2000)
       (is (not (store/get-metadata token-store token-hex))))))
@@ -28,7 +29,7 @@
 (deftest valid-extend-test
   (testing "Test extend works"
     (let [tokens (atom {})
-          token-store (store/->MemTokenStore (token/generate-key) 2 tokens)
+          token-store (store/->MemTokenStore (workflow/generate-key) 2 tokens)
           token-hex (store/create token-store {:username "foo"})]
       (Thread/sleep 1000)
       (store/extend-life token-store token-hex)
@@ -38,6 +39,6 @@
 (deftest destroy-test
   (testing "Test destroying a token"
     (let [tokens (atom {"foo" {}})
-          token-store (store/->MemTokenStore (token/generate-key) 10 tokens)]
+          token-store (store/->MemTokenStore (workflow/generate-key) 10 tokens)]
       (store/destroy token-store "foo")
       (is (= 0 (count (keys @tokens)))))))
